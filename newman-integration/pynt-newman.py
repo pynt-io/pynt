c7227258086e35d81071a323b4066dd97380f0a3
shacharovercast <102512170+shacharovercast@users.noreply.github.com>
2022-10-06 18:01:26 +0300

    Shachar/python helper newman (#6)
    * python script
    
    * no no upload logs
    
    * requierments + usage fix
    
    * args.environment
    
    Co-authored-by: shacharovercast <shachar@pynt.io>

--- newman-integration/pynt-newman.py
+++ newman-integration/pynt-newman.py
@@ -19,8 +19,10 @@ def main():
         if not os.path.exists(c):
             print("Collection file not found")
             exit()
-    if args.e:
-        e = args.e
+
+    if args.environment:
+        e = args.environment
+
         if not os.path.exists(e):
             print("Environment file not found")
             exit()
@@ -37,7 +39,8 @@ def main():
         shutil.copy(c, os.path.join(runningDir, "c.json"))
         
         command = ["-c", "c.json"]
-        if args.e:
+        if args.environment:
+
             shutil.copy(e, os.path.join(runningDir, "e.json"))
             command.extend(["-e", "e.json"])
 
