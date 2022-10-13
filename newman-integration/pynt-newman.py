543867d0dad1f6fb41af7ba87a347c493d2d285e
shacharovercast <shachar@pynt.io>
2022-10-13 11:41:21 +0300

    try in main

--- newman-integration/pynt-newman.py
+++ newman-integration/pynt-newman.py
@@ -6,8 +6,8 @@ import argparse
 import sys
 
 def main():
-    if sys.platform == "win32":
-        os.system('color')
+    # if sys.platform == "win32":
+    #     os.system('color')
 
     parser = argparse.ArgumentParser(usage="pynt-newman.py <postman collection file> -e [postman environment file]")
     parser.add_argument('collection', help="path to your Postman collection")
@@ -33,15 +33,15 @@ def main():
         print("Docker daemon is not running or not installed")
         exit()
 
-    try:
-        image = client.images.pull("ghcr.io/pynt-io/pynt", "newman-latest")
-    except Exception as e:
-        print(e) #Golan's bug- docker was stuck- client was created but unable to run commands.
+    # try:
+    image = client.images.pull("ghcr.io/pynt-io/pynt", "newman-latest")
+    # except Exception as e:
+    #     print(e) #Golan's bug- docker was stuck- client was created but unable to run commands.
 
     with tempfile.TemporaryDirectory(dir = os.getcwd()) as runningDir:
         shutil.copy(c, os.path.join(runningDir, "c.json"))
         
-        command = ["-c", "c.json"]
+        command = ["-c", "c.json","--no-upload-logs"]
         if args.environment:
 
             shutil.copy(e, os.path.join(runningDir, "e.json"))
@@ -51,16 +51,24 @@ def main():
                                         network="host", volumes=[runningDir + ":/etc/pynt"], auto_remove=True)
         output = run.attach(stdout=True, stream=True, logs=True)
         run.start()
-        try:
-            for line in output:
-                try:
-                    decoded = line.decode("utf-8")
-                except UnicodeDecodeError:
-                    decoded = line
-                sys.stdout.write(decoded) ; sys.stdout.flush()
-            run.wait()
-        except Exception as e:
-            print(e) #Ofer's error- most likely docker version
+        # try:
+        for line in output:
+            try:
+                decoded = line.decode("utf-8")
+            except UnicodeDecodeError:
+                decoded = line
+            sys.stdout.write(decoded) ; sys.stdout.flush()
+        run.wait()
+        # except Exception as e:
+        #     print(e) #Ofer's error- most likely docker version
 
 if __name__ == "__main__":
-    main()
+    if sys.platform == "win32":
+        os.system('color')
+        import pywintypes
+        try:
+            main()
+        except pywintypes.error as e:
+            print(e)
+    else:
+        main()
